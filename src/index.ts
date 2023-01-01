440b7799706b3b8c94976298e493160adcba580e
et <et@DESKTOP-554HOOK.localdomain>
2023-01-01 11:00:43 +0100

    format

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,23 @@ import { getHlsExecutable } from './hlsExecutable';
 
 
 export async function activate(context: ExtensionContext): Promise<void> {
-	const config = workspace.getConfiguration('coc-helios');
-	const isEnabled = config.get<boolean>('enable', true)
-	if (!isEnabled) {
-		return
-	}
+   const config = workspace.getConfiguration('coc-helios');
+   const isEnabled = config.get<boolean>('enable', true)
+   if (!isEnabled) {
+      return
+   }
 
-	const serverOptions = getHlsExecutable();
-	const clientOptions = {
-		documentSelector: ['*.hl', 'hl']
-	};
-	const client = new LanguageClient(
-		'coc-helios',
-		'coc-helios',
-		serverOptions,
-		clientOptions
-	);
-	context.subscriptions.push(services.registLanguageClient(client));
+   const serverOptions = getHlsExecutable();
+   const clientOptions = {
+      documentSelector: ['*.hl', 'hl']
+   };
+   const client = new LanguageClient(
+      'coc-helios',
+      'coc-helios',
+      serverOptions,
+      clientOptions
+   );
+   context.subscriptions.push(services.registLanguageClient(client));
 
    client.onReady().then(() => {
       window.showMessage(`${hlsName} v${hlsVersion} running successfully.`);
